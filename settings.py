c94277dafeee43e77cc1261e886d14a046ab9db9
Vlad Sandulescu <vlad.sandulescu@gmail.com>
2014-09-07 12:25:27 +0200

    extracted settings to a new file

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    def __init__(self):
+        pass
+
+    DATASET_FILE = 'dataset/yelp_dataset_challenge_academic_dataset'
+    MONGO_CONNECTION_STRING = "mongodb://localhost:27030/"
+    REVIEWS_DATABASE = "Dataset_Challenge_Reviews"
+    TAGS_DATABASE = "Tags"
+    REVIEWS_COLLECTION = "Reviews"
+    CORPUS_COLLECTION = "Corpus"
