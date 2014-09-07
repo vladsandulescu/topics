d77763480e55c05dd4713917d8bd39d3793d8ed0
Vlad Sandulescu <vlad.sandulescu@gmail.com>
2014-09-07 12:26:46 +0200

    refactored predict class

--- predict.py
+++ predict.py
@@ -2,18 +2,72 @@ import logging
 
 from gensim.models import LdaModel
 from gensim import corpora
+import nltk
+from nltk.stem.wordnet import WordNetLemmatizer
 
 
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+class Predict():
+    def __init__(self):
+        dictionary_path = "models/dictionary.dict"
+        lda_model_path = "models/lda_model_50_topics.lda"
+        self.dictionary = corpora.Dictionary.load(dictionary_path)
+        self.lda = LdaModel.load(lda_model_path)
 
-dictionary_path = "models/dictionary.dict"
-corpus_path = "models/corpus.lda-c"
-lda_num_topics = 50
-lda_model_path = "models/lda_model_50_topics.lda"
+    def load_stopwords(self):
+        stopwords = {}
+        with open('stopwords.txt', 'rU') as f:
+            for line in f:
+                stopwords[line.strip()] = 1
 
-dictionary = corpora.Dictionary.load(dictionary_path)
-corpus = corpora.BleiCorpus(corpus_path)
-lda = LdaModel.load(lda_model_path)
+        return stopwords
 
-for i in lda.show_topics(topics=lda_num_topics):
-    print i
+    def extract_lemmatized_nouns(self, new_review):
+        stopwords = self.load_stopwords()
+        words = []
+
+        sentences = nltk.sent_tokenize(new_review.lower())
+        for sentence in sentences:
+            tokens = nltk.word_tokenize(sentence)
+            text = [word for word in tokens if word not in stopwords]
+            tagged_text = nltk.pos_tag(text)
+
+            for word, tag in tagged_text:
+                words.append({"word": word, "pos": tag})
+
+        lem = WordNetLemmatizer()
+        nouns = []
+        for word in words:
+            if word["pos"] in ["NN", "NNS"]:
+                nouns.append(lem.lemmatize(word["word"]))
+
+        return nouns
+
+    def run(self, new_review):
+        nouns = self.extract_lemmatized_nouns(new_review)
+        new_review_bow = self.dictionary.doc2bow(nouns)
+        new_review_lda = self.lda[new_review_bow]
+
+        print new_review_lda
+
+
+def main():
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+    new_review = "It's like eating with a big Italian family. " \
+                 "Great, authentic Italian food, good advice when asked, and terrific service. " \
+                 "With a party of 9, last minute on a Saturday night, we were sat within 15 minutes. " \
+                 "The owner chatted with our kids, and made us feel at home. " \
+                 "They have meat-filled raviolis, which I can never find. " \
+                 "The Fettuccine Alfredo was delicious. We had just about every dessert on the menu. " \
+                 "The tiramisu had only a hint of coffee, the cannoli was not overly sweet, " \
+                 "and they had this custard with wine that was so strangely good. " \
+                 "It was an overall great experience!"
+
+    predict = Predict()
+    predict.run(new_review)
+
+
+if __name__ == '__main__':
+    main()
+
+
